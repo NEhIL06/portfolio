7d015b7e8a76812705eecb31f0675c384dd15835
Nehil <chnadrakarnehil.06112002@gmail.com>
2026-03-18 16:34:42 +0530

    feat: Add a new `Footer` component with email copy functionality, social links, and navigation links.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/link"
+
 import { useState } from "react"
 import { siteConfig } from "@/lib/site-config"
 import { MagneticButton } from "@/components/motion/magnetic-button"
